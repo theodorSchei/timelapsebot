6467d3c46d548ba450a7ff21c2fe69b753eb963b
theodorSchei <43009539+theodorSchei@users.noreply.github.com>
2022-07-18 20:50:46 +0200

    Added twitter uploads

--- src/instarun.ts
+++ src/instarun.ts
@@ -1,18 +1,22 @@
+import { PostToTwitter } from "./postToTwitter";
+
 const Timelapse = require('./timelapse');
 const Webhotel = require('./uploadToWebHotel');
 
 const runSunsetJob = async () => {
-  console.log(`Running sunset job at ${new Date()}`);
+  const startTime = new Date()
+  console.log(`Running sunset job at ${startTime}`);
   try {
     const timelapsePath = await Timelapse.create(50000, 10000, true);
 
-    const uploadStatus = await Webhotel.uploadFile(timelapsePath, 'video/folder/dagens.mp4');
-
+    const uploadStatus = await PostToTwitter(timelapsePath, startTime.toUTCString());
+    
     if (uploadStatus) {
-      console.log('UPLOAD SUCCESSFUL!!!!!!!!!');
+      console.log("Tweet success!");
     } else {
-      console.log('UPLOAD FAILED');
+      console.log("Tweet failed :(");
     }
+      
     console.log(`Sunset job finished at ${new Date()}`);
   } catch (e) {
     console.error('Something went wrong');
---
+++ src/postToTwitter.ts
@@ -0,0 +1,38 @@
+import { TwitterApi } from 'twitter-api-v2';
+import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+export const PostToTwitter = async (videoPath: string, tweetText: string): Promise<boolean> => {
+  console.log('Posting to twitter');
+
+  const client = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY!,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET_KEY!,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN!,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+  });
+
+  console.log('Client created');
+
+  console.log('Uploading video');
+  const mediaIds = await Promise.all([
+    // file path
+    client.v1.uploadMedia(videoPath, { type: 'longmp4' }),
+  ]);
+
+  console.log('Video uploaded');
+
+  console.log('Posting tweet');
+  return await client.v1.tweet(tweetText, { media_ids: mediaIds }).then(
+    (res) => {
+      console.log(res);
+      console.log(`Successfully posted tweet!!!!`);
+      return true;
+    },
+    (err) => {
+      console.log(err);
+      return false
+    }
+  );
+};
+
+PostToTwitter('/Users/theodor/dev/twitterbot/timelapse.mp4', 'Testing');
